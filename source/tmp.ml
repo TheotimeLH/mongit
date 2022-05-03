0b6b1e1a92c8b3a74bfb844e4d388db762d577dd
Theotime <theotime.lh@gmail.com>
2022-05-03 23:55:23 +0200

    teste de la fonction de diff de git 5

--- source/tmp.ml
+++ source/tmp.ml
@@ -1,25 +1,15 @@
 open Root
 
 let aff_qlqch str =
-(* retest *)
-(* test *)
-(* TEST DEBUG *)
-(*print_debug "Affiche du debug \n" ; *)
-(* TEST DEBUG *)
-(*print_debug "Affiche du debug \n" ; *)
-(* blalba*)
-(* test *)
-(* TEST DEBUG *)
-(*print_debug "Affiche du debug \n" ; *)
 (* TEST DEBUG *)
 (*print_debug "Affiche du debug \n" ; *)
 (* TEST FILEPERM *)
-(*let file = "../sysnum/.git" in
-  let st = Unix.stat file in
-  Printf.printf "num de perm de %s : %d \n" file st.st_perm *)
 (* TEST repo_find 
   let s = Outils.repo_find () in
   Printf.printf "%s" s ; *)
+(*let file = "../sysnum/.git" in
+  let st = Unix.stat file in
+  Printf.printf "num de perm de %s : %d \n" file st.st_perm *)
 (* TEST Hash
   let key,rest = Outils.hash_file 
     "/home/theotime/Documents/Projets/mongit/source/tmp.ml" in 
